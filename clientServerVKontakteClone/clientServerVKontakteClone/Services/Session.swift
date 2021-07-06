9c7482aff1b54b3cc2b5bf345fa7e146965f78ef
Denis <recmanus@gmail.com>
2021-07-06 10:55:50 +0300

    Lesson-1-HomeWork
    The new project was created and the lesson 1 homework was done.

---
+++ clientServerVKontakteClone/clientServerVKontakteClone/Services/Session.swift
@@ -0,0 +1,18 @@
+//
+//  Session.swift
+//  clientServerVKontakteClone
+//
+//  Created by Denis Kazarin on 06.07.2021.
+//
+
+import Foundation
+
+// MARK: - Singletone Session
+
+final class Session {
+    static let shared = Session()
+    private init() {}
+    
+    var token: String = ""
+    var userId: Int = 0
+}
