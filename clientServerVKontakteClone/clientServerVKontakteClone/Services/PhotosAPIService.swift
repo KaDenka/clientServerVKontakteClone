4fd2f4c2c91247390dfecb65128ee3da60855b97
Denis <recmanus@gmail.com>
2021-09-09 19:45:23 +0300

    Lesson6_UITV_UICV_Practice_HomeWork
    Lesson 6 Home Work

--- clientServerVKontakteClone/clientServerVKontakteClone/Services/PhotosAPIService.swift
+++ clientServerVKontakteClone/clientServerVKontakteClone/Services/PhotosAPIService.swift
@@ -9,30 +9,32 @@ import Foundation
 
 class PhotosAPIService {
     func photosListAPIRequest( complition: @escaping ([Photo]) -> (Void) ) {
-        var requestConstructor = URLComponents()
-        requestConstructor.scheme = "https"
-        requestConstructor.host = "api.vk.com"
-        requestConstructor.path = "/method/photos.get"
-        requestConstructor.queryItems = [
-            URLQueryItem(name: "user_id", value: "\(Session.shared.userId)"),
-            URLQueryItem(name: "album_id", value: "wall"),
-            URLQueryItem(name: "access_token", value: "\(Session.shared.token)"),
-            URLQueryItem(name: "v", value: "\(Session.shared.versionVK)")
-        ]
-        let request = URLRequest(url: requestConstructor.url!)
-        
-        let session = URLSession.shared
-        let task = session.dataTask(with: request) { (data, response, error) in
-            guard let data = data else {return}
-            do { let responseData = try JSONDecoder().decode(Photos.self, from: data)
-                let photoCollection = responseData.response.items
-                DispatchQueue.main.async {
-                    complition(photoCollection)
+        DispatchQueue.global(qos: .userInteractive).async {
+            var requestConstructor = URLComponents()
+            requestConstructor.scheme = "https"
+            requestConstructor.host = "api.vk.com"
+            requestConstructor.path = "/method/photos.get"
+            requestConstructor.queryItems = [
+                URLQueryItem(name: "user_id", value: "\(Session.shared.userId)"),
+                URLQueryItem(name: "album_id", value: "wall"),
+                URLQueryItem(name: "access_token", value: "\(Session.shared.token)"),
+                URLQueryItem(name: "v", value: "\(Session.shared.versionVK)")
+            ]
+            let request = URLRequest(url: requestConstructor.url!)
+            
+            let session = URLSession.shared
+            let task = session.dataTask(with: request) { (data, response, error) in
+                guard let data = data else {return}
+                do { let responseData = try JSONDecoder().decode(Photos.self, from: data)
+                    let photoCollection = responseData.response.items
+                    DispatchQueue.main.async {
+                        complition(photoCollection)
+                    }
+                } catch {
+                    print(error)
                 }
-            } catch {
-                print(error)
             }
+            task.resume()
         }
-        task.resume()
     }
 }
